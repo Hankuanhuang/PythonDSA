d159febca3ec00bb52f361ebb1b4523539f59a1a
Ken Hunag <ken861206@gmail.com>
2026-07-28 16:12:34 -0400

    Complete Python variables and data types

---
+++ 01_PythonBasics/02_data_types.py
@@ -0,0 +1,29 @@
+age = 28
+
+print(type(age))
+
+height = 186.5
+
+print(type(height))
+
+name = "Ken"
+
+print(type(name))
+
+student = True
+
+print(type(student))
+
+city = "North York"
+temperature = 25.6
+population = 3000000
+canada = True
+
+print(type(city))
+print(type(temperature))
+print(type(population))
+print(type(canada))
+
+print(float("5.5"))
+print(str(100))
+print(int("Ken"))
