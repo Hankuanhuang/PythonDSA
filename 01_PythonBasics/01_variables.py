a814cf5c2748ac6f06aa255a1b55f59f93bbb3ed
Ken Hunag <ken861206@gmail.com>
2026-07-28 16:03:48 -0400

    Complete Python variables lesson

---
+++ 01_PythonBasics/01_variables.py
@@ -0,0 +1,41 @@
+age = 20
+print(age)
+
+name = "ken"
+print(name)
+
+height = 186.5
+print(height)
+
+student = True 
+print(student)
+#Ture and False have to be capital
+
+city = "North York"
+country = "Toronto"
+weight = 90
+is_student = True
+
+print(city)
+print(country)
+print(weight)
+print(is_student)
+
+name = "ken"
+age = 28
+height = 186.5
+major = "Computer programming"
+gpa = 3.8
+
+
+#10 + 5          ✅ int + int
+
+#"10" + "5"      ✅ str + str
+
+#"Hello " + "Ken" ✅ str + str
+
+#"Hello " + 10   ❌ str + int（錯誤）
+
+age = 28
+
+print(f"I am {age}")
