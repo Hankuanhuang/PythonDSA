164ec50027f05e132ac080550bdc5d8fea6745ca
Ken Hunag <ken861206@gmail.com>
2026-07-29 10:11:32 -0400

    Complete Python basics: if statements

---
+++ 01_PythonBasics/05_if.py
@@ -0,0 +1,34 @@
+# age = 20
+
+# if age >= 18:       # have a : 
+#     print("Adult")
+
+
+# age = 15
+# if age >= 18:
+#     print("Adult")
+
+# print("Done") # here have not else
+
+# age = int(input("Age: "))
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+
+# score = int(input("Score: "))
+
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# else:
+#     print("C")
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
