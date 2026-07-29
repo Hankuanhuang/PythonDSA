00eeab8a1873bd09044f7ac9d5f516b21e177e07
Ken Hunag <ken861206@gmail.com>
2026-07-29 09:53:26 -0400

    Complete Python basics: variables, data types, and print

---
+++ 01_PythonBasics/03_print.py
@@ -0,0 +1,40 @@
+print("Hello World")
+print("My name is ken.")
+print(100)
+print(3.14)
+
+
+name = "Ken"
+age = 20
+Height = 185.5
+
+print(name)
+print(age)
+print(Height)
+
+print(name, age, Height)
+
+print("My name is", name)
+print("I'm", age, "years old")
+
+print(name, age, Height, sep="-") # this means seprate by.
+
+
+print("Hello", end =" ") # this end will cut the next line
+print ("World")
+#end="\n" -> switch line
+
+print("I'm learning \" Python \". ")
+print("C: \\Users\\ken")
+
+
+name = "Ken"
+school = "Seneca"
+program = "CPA"
+
+print("==================")
+print("Student Information")
+print("==================")
+print("Name:", name)
+print("School:", school)
+print("program:", program)
