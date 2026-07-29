6f6797b467bc99f37921dbcba83c79aa2630ae9e
Ken Hunag <ken861206@gmail.com>
2026-07-29 10:01:28 -0400

    Complete Python basics: input

---
+++ 01_PythonBasics/04_input.py
@@ -0,0 +1,19 @@
+# name = input("Enter your name: ")
+
+# print("Hello", name)
+
+# age = input("Enter your age: ")
+
+# print(age)
+
+# age = input("Age: ")
+# print(age+age)
+
+# age = int(input("Age: "))
+# print(age+ age)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("next your you will be ", age + 1)
