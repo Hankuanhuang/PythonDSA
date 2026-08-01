9fbca014527f55505e1293b1d6c1ca65db905ac5
Ken Hunag <ken861206@gmail.com>
2026-08-01 10:26:31 -0400

    Complete Linked List Stack implementation in Python with push, pop, peek, and isEmpty

---
+++ 04_Stack/05-isEmpty.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+head = None
+
+def push(data):
+    global head
+
+    newNode = Node(data)
+    newNode.next = head
+    head = newNode
+
+def pop():
+    global head
+
+    if head is None:
+        return
+    head = head.next
+
+def peek():
+    global head
+
+    if head is None:
+        return
+    
+    return head.data
+
+
+def isEmpty():
+    global head
+
+    if head is None:
+        return True
+    return False
+
+
+push(10)
+push(20)
+push(30)
+
+current = head
+
+while current is not None:
+    print(current.data)
+    current = current.next
